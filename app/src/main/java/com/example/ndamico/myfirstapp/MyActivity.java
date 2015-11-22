ddf3a4964bd7da67147b0e38ffb8c4b327fcd02c
Nisha D'Amico <nvdamico@gmail.com>
2015-11-22 13:51:14 -0500

    First commit! Added new activity to send info to new page

--- app/src/main/java/com/example/ndamico/myfirstapp/MyActivity.java
+++ app/src/main/java/com/example/ndamico/myfirstapp/MyActivity.java
@@ -1,5 +1,6 @@
 package com.example.ndamico.myfirstapp;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
@@ -8,8 +9,11 @@ import android.support.v7.widget.Toolbar;
 import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.EditText;
 
 public class MyActivity extends AppCompatActivity {
+
+    public final static String EXTRA_MESSAGE = "com.example.ndamico.myfirstapp.MESSAGE";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -36,7 +40,7 @@ public class MyActivity extends AppCompatActivity {
     }
 
     @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
+         public boolean onOptionsItemSelected(MenuItem item) {
         // Handle action bar item clicks here. The action bar will
         // automatically handle clicks on the Home/Up button, so long
         // as you specify a parent activity in AndroidManifest.xml.
@@ -48,5 +52,14 @@ public class MyActivity extends AppCompatActivity {
         }
 
         return super.onOptionsItemSelected(item);
+    }
+
+    public void sendInfo(View view){
+        //TODO sends entered info when button is clicked
+        Intent intent = new Intent(this, DisplayMessageActivity.class);
+        EditText weight = (EditText) findViewById(R.id.input_weight);
+        String weightInput = weight.getText().toString();
+        intent.putExtra(EXTRA_MESSAGE, weightInput);
+        startActivity(intent);
     }
 }
